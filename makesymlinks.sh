a5dd900608e0a0c26bc60e1851fa5d4066b59074
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2018-01-14 00:20:23 +0100

    Change name folder and include color scheme for gnome terminal

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 ############################
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/.dotfiles
+# This script ->
+# # creates symlinks from the home directory to any desired dotfiles in ~/.dotfiles
+# # save a backup of old dotfiles in case of overwritting
+# # set solarized Colorscheme for Gnome Terminal
+# # install zsh and oh-my-zsh 
 ############################
 
 ########## Variables
@@ -9,18 +13,18 @@
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
 files="zshrc oh-my-zsh tmux.conf vscode"    # list of files/folders to symlink in homedir
-
+theme="prose.zsh-theme"
 ##########
 
 # create dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
-echo "done"
+echo "Done"
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
 cd $dir
-echo "done"
+echo "Done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
@@ -29,6 +33,15 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+# set solarized color scheme for Gnome Terminal
+echo -n "Setting solarized Colorscheme for Gnome Terminal ..."
+sudo apt-get install dconf-cli
+if [[ ! -d $dir/gnome-terminal-colors-solarized/ ]]; then
+    git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
+fi
+./gnome-terminal-colors-solarized/install.sh
+echo "Done"
 
 install_zsh () {
 # Test to see if zshell is installed.  If it is:
@@ -41,6 +54,8 @@ if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     if [[ ! -d $dir/zsh-syntax-highlighting/ ]]; then
         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
     fi
+    # Copy theme into the right folder
+    cp $dir/$theme $dir/oh-my-zsh/custom/$theme
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
         chsh -s $(which zsh)
